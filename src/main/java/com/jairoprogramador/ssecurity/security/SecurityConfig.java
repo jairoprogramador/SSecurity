bd4b85b54efb54fae831c92eb5f30ddba81ea404
Jairo Galvez <jairo.jggc@gmail.com>
2023-09-22 23:04:25 -0500

    data base personalized

--- src/main/java/com/jairoprogramador/ssecurity/security/SecurityConfig.java
+++ src/main/java/com/jairoprogramador/ssecurity/security/SecurityConfig.java
@@ -4,13 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -26,11 +22,6 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
-    }
-
-    @Bean
-    UserDetailsService userDetailsService(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
