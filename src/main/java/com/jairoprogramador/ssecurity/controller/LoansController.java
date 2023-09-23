2997815e21ee620bc8fc8993757516363becd3ab
Jairo Galvez <jairo.jggc@gmail.com>
2023-09-22 20:08:29 -0500

    fundamentos spring security

---
+++ src/main/java/com/jairoprogramador/ssecurity/controller/LoansController.java
@@ -0,0 +1,24 @@
+package com.jairoprogramador.ssecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/loads")
+public class LoansController {
+    @GetMapping
+    public Map<String, String> loans() {
+        return Collections.singletonMap("msj", "loans");
+    }
+
+    @PostMapping
+    public Object methodoPostTest() {
+        return "hello world they are a test method";
+    }
+}
---
+++ src/main/java/com/jairoprogramador/ssecurity/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.jairoprogramador.ssecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests
+                        ( auth ->
+                                auth
+                                        .requestMatchers("/loans", "/balance", "/accounts","/cards").authenticated()
+                                        .anyRequest().permitAll()
+                        )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+}
