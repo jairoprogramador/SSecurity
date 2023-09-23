591f84f3e6ed97252912d6532d9aac6ef0aaa30b
Jairo Galvez <jairo.jggc@gmail.com>
2023-09-22 20:29:55 -0500

    in memory

--- src/main/java/com/jairoprogramador/ssecurity/security/SecurityConfig.java
+++ src/main/java/com/jairoprogramador/ssecurity/security/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,6 +24,26 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        var admin = User.withUsername("admin")
+                .password("to_be_encoded")
+                .authorities("ADMIN")
+                .build();
+
+        var user = User.withUsername("user")
+                .password("to_be_encoded")
+                .authorities("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
