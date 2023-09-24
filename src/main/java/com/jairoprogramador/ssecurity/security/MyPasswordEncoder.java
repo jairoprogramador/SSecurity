af509d7e55ca7f97d80141545fba0d8cb22119c9
Jairo Galvez <jairo.jggc@gmail.com>
2023-09-24 11:09:56 -0500

    my password encoder

---
+++ src/main/java/com/jairoprogramador/ssecurity/security/MyPasswordEncoder.java
@@ -0,0 +1,18 @@
+package com.jairoprogramador.ssecurity.security;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyPasswordEncoder implements PasswordEncoder {
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return String.valueOf(rawPassword.toString().hashCode());
+    }
+
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        var passwordAsString = String.valueOf(rawPassword.toString().hashCode());
+        return encodedPassword.equals(passwordAsString);
+    }
+}
--- src/main/java/com/jairoprogramador/ssecurity/security/SecurityConfig.java
+++ src/main/java/com/jairoprogramador/ssecurity/security/SecurityConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,11 +20,6 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
     }
 
 }
